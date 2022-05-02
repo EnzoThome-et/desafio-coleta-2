a625327fbb77c5e2415e2a293a51707c30835699
enzo <enzo@pop-os.localdomain>
2022-05-02 19:47:59 -0300

    feat: initial implementation of resultPage

---
+++ app/frontend/src/pages/ResultPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ResultPage = () => {
+	return (
+		<>
+        Positiva: 4
+        Negativa: 0
+        Não Avaliada: 0
+		</>
+	);
+};
+
+export default ResultPage;
