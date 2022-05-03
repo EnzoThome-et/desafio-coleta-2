2a5110143987ac01420f90d3200e94f284dd76ed
enzo <enzo@pop-os.localdomain>
2022-05-02 22:53:39 -0300

    refactor: rename Question interface to IQuestions

--- app/backend/src/interfaces/Questions.ts
+++ app/backend/src/interfaces/Questions.ts
@@ -1,6 +1,8 @@
-export interface Questions {
+interface IQuestions {
     'Pergunta1': string,
     'Pergunta2': string,
     'Pergunta3': string,
     'Pergunta4': string,
 }
+
+export default IQuestions;
