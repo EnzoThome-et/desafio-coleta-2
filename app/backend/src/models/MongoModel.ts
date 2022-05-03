2b995370f06aace8961be70e0c6b55797ee8a94d
enzo <enzo@pop-os.localdomain>
2022-05-02 21:30:02 -0300

    feat: MongoModel class

---
+++ app/backend/src/models/MongoModel.ts
@@ -0,0 +1,13 @@
+import { Model as M, Document } from 'mongoose';
+
+import { Model } from '../interfaces/ModelInterface';
+
+abstract class MongoModel<T> implements Model<T> {
+	constructor(protected model: M<T & Document>) {}
+
+	create = async (obj: T): Promise<T> => this.model.create({...obj});
+
+	read = async (): Promise<T[]> => this.model.find();
+}
+
+export default MongoModel;
