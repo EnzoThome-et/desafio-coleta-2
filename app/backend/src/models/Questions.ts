de0066c04f4338f89572a3a0881ef02db8d1da45
enzo <enzo@pop-os.localdomain>
2022-05-02 23:29:05 -0300

    refactor: remove versionKey and rename a property

--- app/backend/src/models/Questions.ts
+++ app/backend/src/models/Questions.ts
@@ -5,6 +5,9 @@ const QuestionsSchema = new Schema({
 	Pergunta2: String,
 	Pergunta3: String,
 	Pergunta4: String,
-});
+	QuantidadePositiva: Number,
+	QuantidadeNegativa: Number,
+	QuantidadeNaoAvaliada: Number,
+}, { versionKey: false });
 
 export default model('Questions', QuestionsSchema);
