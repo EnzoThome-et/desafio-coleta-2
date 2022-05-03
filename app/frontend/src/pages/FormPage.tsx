a09f3529ad29171ff08b53860907bca33afe0d9f
enzo <enzo@pop-os.localdomain>
2022-05-03 15:30:37 -0300

    style: formPage css

--- app/frontend/src/pages/FormPage.tsx
+++ app/frontend/src/pages/FormPage.tsx
@@ -1,52 +1,63 @@
 import React, {useState} from 'react';
 import { textAreaCounter } from '../services/counter';
 import { useForm } from 'react-hook-form';
+import '../styles/form.css';
 import '../styles/button.css';
 
 const Page = () => {
-	const { register, handleSubmit } = useForm();
+	const { register, formState: { errors }, handleSubmit } = useForm();
 	const [data, setData] = useState('');
 	return (
-		<>
+		<main className="formContainer">
 			<form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
 				<h2> Desafio Coleta - GRX </h2>
 
-				<div className="button">
-					<input type="radio" id="SR1" value="Sim" {...register('Pergunta1')} />
-					<label htmlFor="SR1">Sim</label>
+				<p>1) Você se considera bom em lógica? </p>
+				<div className="firstQuestionContainer">
+					<div className="button">
+						<input type="radio" id="SR1" value="Sim" {...register('Pergunta1', {required: true})} />
+						<label htmlFor="SR1">Sim</label>
+					</div>
+					<div className="button">
+						<input type="radio" id="NR1" value="Não" {...register('Pergunta1', {required: true})} />
+						<label htmlFor="NR1">Não</label>
+					</div>
 				</div>
-				<div className="button">
-					<input type="radio" id="NR1" value="Não" {...register('Pergunta1')} />
-					<label htmlFor="NR1">Não</label>
+				{errors.Pergunta1?.type === 'required' && 'Resposta obrigatória'}
+				
+				<p>2) Gosta de aprender com desafios? </p>
+				<div className="secondQuestionContainer">
+					<div className="button">
+						<input type="radio" id="SR2" value="Sim" {...register('Pergunta2', {required: true})} />
+						<label htmlFor="SR2">Sim</label>
+					</div>
+					<div className="button">
+						<input type="radio" id="NR2" value="Não" {...register('Pergunta2', {required: true})} />
+						<label htmlFor="NR2">Não</label>
+					</div>
 				</div>
-
-				<div className="button">
-					<input type="radio" id="SR2" value="Não" {...register('Pergunta2')} />
-					<label htmlFor="SR2">Sim</label>
-				</div>
-				<div className="button">
-					<input type="radio" id="NR2" value="Não" {...register('Pergunta2')} />
-					<label htmlFor="NR2">Não</label>
-				</div>
-
+				{errors.Pergunta2?.type === 'required' && 'Resposta obrigatória'}
+				
 				<p data-testid="question-three"> 3) Gostaria de fazer parte da GRX? </p>
-				<select {...register('Pergunta3')}>
+				<select {...register('Pergunta3', { required: true })}>
 					<option value="">Selecione...</option>
 					<option value="Sim">Sim</option>
 					<option value="Não">Não</option>
 					<option value="Não Sei">Não Sei</option>
 					<option value="Agora !!">Agora !!</option>
 				</select>
+				{errors.Pergunta3?.type === 'required' && 'Selecione uma opção'}
 
-				<p data-testid="question-four"> 4) Por favor, justifique a resposta anterior </p>
-				<textarea {...register('Pergunta4')} className="textarea" id="fourth-response" minLength={15} maxLength={200} onChange={() => { textAreaCounter();}}/>
+				<p data-testid="question-four" className="question-four"> 4) Por favor, justifique a resposta anterior </p>
+				<textarea {...register('Pergunta4', {required: true})} className="textarea" id="fourth-response" minLength={15} maxLength={200} onChange={() => { textAreaCounter();}}/>
 				<p className="counter">200</p>
+				{errors.Pergunta4?.type === 'required' && 'Justifique sua resposta!'}
 
-				<button type="submit">Enviar</button>
+				<button type="submit" className="submit">Enviar</button>
 				<p>{data}</p>
 			</form>
 			
-		</>
+		</main>
 	);
 };
 
